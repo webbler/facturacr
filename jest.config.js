86ff984ecbcc6d957f75b5d096b5873f3f681f8c
webbler <36542501+webbler@users.noreply.github.com>
2018-08-14 09:46:56 -0600

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bail: true,
-  // verbose: true,
-  collectCoverage: true,
-  setupFiles: ['./jest.shim.js', './jest.setup.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/test/'
-  ]
-};
